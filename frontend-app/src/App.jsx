2ae4afbc5fa1c3b8f68266ce5016c7f322010be0
joeban <joeban@twbeex.com>
2024-09-26 18:47:44 +0800

    feat: frontend add real router

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,34 +1,114 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+import { DOMAIN, SUB_DOMAINS } from "./constant/subDomains";
 
 function App() {
   return (
-    <main>
-      <h1>SEO example: {DOMAIN}</h1>
-      <a href="https://masaya365casino.win" style={{ display: "none" }}>
-        masaya365casino.win
-      </a>
-      <ul style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
-        {links.map((link) => {
-          return (
-            <a href={link} key={link} style={{ textAlign: "start" }}>
-              <li>{link}</li>
-            </a>
-          );
-        })}
-      </ul>
-    </main>
+    <Router>
+      <main>
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Outlet />
+                </Layout>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path="news" element={<News />} />
+              <Route path="games" element={<Games />} />
+              <Route path="sports" element={<Sports />} />
+            </Route>
+          </Routes>
+        </div>
+      </main>
+    </Router>
   );
 }
 
 export default App;
 
-const DOMAIN = "masaya365casino.win";
-const links = [
-  `https://masaya365casino.win/masaya365-new-2024/`,
-  `https://masaya365casino.win/2024/09/02/masaya365-100bonus-firstdeposit/`,
-  `https://masaya365casino.win/2024/08/23/masaya365-win1000k-cockfighting-prize/`,
-  `https://masaya365casino.win/2024/08/19/bccomebonus365member/`,
-  `https://masaya365casino.win/2024/08/12/articlesharing-13/`,
-  `https://masaya365casino.win/2024/08/12/articlesharing-12/`,
-  `https://masaya365casino.win/2024/08/12/articlesharing-11/`,
-];
+const Home = () => {
+  return (
+    <ul style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+      {SUB_DOMAINS.map((link) => {
+        return (
+          <li
+            key={link}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "left",
+            }}
+          >
+            <Link to={link}>{`https://${DOMAIN}${link}`}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const News = () => {
+  return (
+    <div>
+      <h2>News Page</h2>
+    </div>
+  );
+};
+
+const Games = () => {
+  return (
+    <div>
+      <h2>Games Page</h2>
+    </div>
+  );
+};
+
+const Sports = () => {
+  return (
+    <div>
+      <h2>sports Page</h2>
+    </div>
+  );
+};
+
+const Layout = ({ children }) => {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <nav
+        style={{
+          display: "flex",
+          gap: "12px",
+          background: "white",
+          padding: "12px",
+        }}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/news">News</Link>
+        <Link to="/games">Games</Link>
+        <Link to="/sports">Sports</Link>
+      </nav>
+      <main
+        style={{
+          width: "100%",
+          height: "calc(100dvh - 48px)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>SEO example site: {DOMAIN}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
---
+++ frontend-app/src/constant/subDomains.js
@@ -0,0 +1,2 @@
+export const SUB_DOMAINS = [`/news`, `/games`, `/sports`];
+export const DOMAIN = "masaya365casino.win";
